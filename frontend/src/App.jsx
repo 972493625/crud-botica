7f539edc1b98453b402059f42764cd6f28e1b157
equipo <equipotrabajo123@gmail.com>
2025-04-28 09:00:00 -0500

    servidor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+//actualizacion de server
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
